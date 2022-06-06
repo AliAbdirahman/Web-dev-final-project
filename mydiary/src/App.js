050239bee6ea7692e67f68c42ec5ad5408c6ae8d
AliAbdirahman <aliabdirahman13579@gmail.com>
2022-06-06 16:45:18 +0700

    Add your commit

---
+++ mydiary/src/App.js
@@ -0,0 +1,25 @@
+
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import './App.css';
+import Header from './components/Header'
+import NotesList from './pages/NotesList'
+import NotePage from './pages/NotePage'
+function App() {
+
+  return (
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Route path="/" exact component={NotesList} />
+          <Route path="/note/:id" component={NotePage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ mydiary/src/components/ListItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+let getTime = (note) => {
+  return new Date(note.updated).toLocaleDateString()
+}
+
+let getTitle = (note) => {
+  const title = note.body.split('\n')[0]
+  return title
+}
+const ListItem = ({ note }) => {
+  return (
+    <Link to={`/note/${note.id}`}>
+      <div className="notes-list-item">
+        <h3>{getTitle(note)}</h3>
+        <p><span>{getTime(note)}</span></p>
+      </div>
+        
+    </Link>
+  )
+}
+
+export default ListItem
